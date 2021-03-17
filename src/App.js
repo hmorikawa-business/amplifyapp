50188f0eff11561e8da0d67a42034d2b74ef124e
MorikawaHiroki <hmorikawa@skywill.jp>
2021-03-17 20:42:12 +0900

    add auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
@@ -15,11 +16,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hoge
         </a>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
